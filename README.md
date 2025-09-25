fd2df364165e1930f20e90b9def30a1d03432eaa
Athya <athyauk@gmail.com>
2025-09-24 20:37:36 -0700

    Update README and clean up code/comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # recommender-system
 Simple recommender system using Kafka (with simulated events) and Spark
+
+To start the environment, run $ docker compose up -d --build
+
+To start Kafka producer, run $ docker exec -it python-client python kafka-producer.py
+
+To start spark stream processing, run:
+
+$ docker exec -it spark-controller   spark-submit   --master spark://spark-controller:7077   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,org.elasticsearch:elasticsearch-spark-30_2.12:9.1.4 --conf "spark.executor.extraJavaOptions=-Dorg.elasticsearch.hadoop.rest.logging.level=debug" /opt/spark-apps/spark-consumer.py
+
+Used this article as a reference for the Kafka + Spark set up: https://medium.com/@yaduvanshineelam09/recommendation-system-using-pyspark-kafka-and-spark-streaming-ba43201ff4bd
--- spark-apps/spark-consumer.py
+++ spark-apps/spark-consumer.py
@@ -6,43 +6,28 @@ KAFKA_TOPIC_NAME_CONS = "movielence"
 KAFKA_OUTPUT_TOPIC_NAME_CONS = "outputtopic"
 KAFKA_BOOTSTRAP_SERVERS_CONS = 'kafka:9092'
 
-# TODO: think about this
 schema = StructType() \
     .add("movie_id", IntegerType()) \
     .add("title", StringType()) \
     .add("genres", ArrayType(StringType()))
 
 if __name__ == "__main__":
-    print("PySpark Structured Streaming with Kafka Application Started ...")
     spark = SparkSession \
              .builder \
-             .appName("PySpark Structured Streaming with Kafka Demo") \
+             .appName("Spark Processing") \
             .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
-    print(" kafka Started ...")
     # Construct a streaming DataFrame that reads from testtopic
     df = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers",                  KAFKA_BOOTSTRAP_SERVERS_CONS) \
+        .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS_CONS) \
         .option("subscribe", KAFKA_TOPIC_NAME_CONS) \
-        .load()    
-    print("Printing Schema of transaction_detail_df: ")
-    df.printSchema()
-    # Write final result into console for debugging purpose
-    # trans_detail_write_stream = transaction_detail_df \
-    #    .writeStream \
-    #     .trigger(processingTime='1 seconds') \
-    #     .outputMode("update") \
-    #     .option("truncate", "false")\
-    #     .format("console") \
-    #     .start()
-    # trans_detail_write_stream.awaitTermination()
+        .load()
 
     parsed = df.selectExpr("CAST(value AS STRING)") \
         .select(from_json(col("value"), schema).alias("data")) \
         .select("data.*")
-    print(parsed)
 
     query = parsed.writeStream \
         .outputMode("append") \
